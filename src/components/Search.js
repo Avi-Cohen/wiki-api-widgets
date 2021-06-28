5887d5bd8a9429830feaefdac90816e920d3c724
Avi-Cohen <aviko.cohen@gmail.com>
2021-06-28 14:30:43 +0300

    cleared form at init
    :s
    :qa!

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,30 +2,56 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Search() {
-  const [term, setTerm] = useState("swimming");
+  const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    (async () => {
+    const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
-          format: "json",
           origin: "*",
+          format: "json",
           srsearch: term,
         },
       });
+
       setResults(data.query.search);
-    })();
+    };
+
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            rel="noreferrer"
+            target="_blank"
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
@@ -49,4 +75,3 @@ export default function Search() {
     </div>
   );
 }
-
