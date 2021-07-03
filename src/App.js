e986c13bf90797b8e8f3f21a844e5c2816b1e9a7
Avi-Cohen <aviko.cohen@gmail.com>
2021-07-03 14:51:02 +0300

    added a dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 export default function App() {
+  const options = [
+    { label: "The color Red", value: "red" },
+    { label: "The color Green", value: "green" },
+    { label: "The color Blue", value: "blue" },
+    { label: "The color Orange", value: "orange" },
+    { label: "The color Black", value: "black" },
+    { label: "The color Purple", value: "purple" },
+  ];
+  const [selected, setSelected] = useState(options[0]);
   return (
     <>
       <h1>Widgets App</h1>
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
       <Search />
     </>
   );
